2bfcfc09f7541918efe50ed3e7d6878357df537e
Isiah Jones <isiah.jones33@outlook.com>
2021-08-21 14:26:59 -0400

    Small Updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const handOptions = {
-    "rock": "./assets/Rock.png",
-    "paper": "./assets/Paper.png",
-    "scissors": "./assets/Scissors.png"
+    "rock": "/assets/Rock.png",
+    "paper": "/assets/Paper.png",
+    "scissors": "/assets/Scissors.png"
 }
 
 let SCORE = 0;
@@ -13,68 +13,68 @@ const pickUserHand = (hand) => {
     let contest = document.querySelector(".contest");
     contest.style.display = "flex";
 
+    // set user Image
     document.getElementById("userPickImage").src = handOptions[hand];
 
-    let houseHand = pickHouseHand();
+    pickComputerHand(hand);
+};
+
+const pickComputerHand = (hand) => {
+    let hands = ["rock", "paper", "scissors"];
+    let houseHand = hands[Math.floor(Math.random() * hands.length)];
+
+    // set computer image 
+    document.getElementById("housePickImage").src = handOptions[houseHand]
 
     referee(hand, houseHand);
-}
-
-const pickHouseHand = () => {
-    let hands = ["rock", "paper", "scissors"];
-    let houseHand = hands[Math.floor(Math.random() * 3)];
-
-    document.getElementById("housePickImage").src = handOptions[houseHand];
-
-    return houseHand;
-}
+};
 
 const referee = (userHand, houseHand) => {
     if (userHand == "paper" && houseHand == "scissors") {
-        setResult("YOU LOSE!");
+        getResult("YOU LOSE!");
     }
     if (userHand == "paper" && houseHand == "rock") {
-        setResult("YOU WIN!");
+        getResult("YOU WIN!");
         setScore(SCORE + 1);
     }
     if (userHand == "paper" && houseHand == "paper") {
-        setResult("It's a tie!");
+        getResult("It's a tie!");
     }
     if (userHand == "rock" && houseHand == "scissors") {
-        setResult("YOU WIN!");
+        getResult("YOU WIN!");
         setScore(SCORE + 1);
     }
     if (userHand == "rock" && houseHand == "paper") {
-        setResult("YOU LOSE!");
+        getResult("YOU LOSE!");
     }
     if (userHand == "rock" && houseHand == "rock") {
-        setResult("It's a tie!");
+        getResult("It's a tie!");
     }
     if (userHand == "scissors" && houseHand == "scissors") {
-        setResult("It's a tie!");
+        getResult("It's a tie!");
     }
     if (userHand == "scissors" && houseHand == "rock") {
-        setResult("YOU LOSE!");
+        getResult("YOU LOSE!");
     }
     if (userHand == "scissors" && houseHand == "paper") {
-        setResult("YOU WIN!");
+        getResult("YOU WIN!");
         setScore(SCORE + 1);
     }
-}
-
-const setResult = (result) => {
-    document.querySelector(".result h1").innerHTML = result;
-}
-
-const setScore = (score) => {
-    SCORE = score;
-    document.querySelector(".score h1").innerHTML = score;
-}
+};
 
 const restartGame = () => {
-    let hands = document.querySelector(".hands");
-    hands.style.display = "flex";
-
     let contest = document.querySelector(".contest");
     contest.style.display = "none";
+
+    let hands = document.querySelector(".hands");
+    hands.style.display = "flex";
+}
+
+const getResult = (result) => {
+    document.querySelector(".result h1").innerText = result;
+}
+
+const setScore = (newScore) => {
+    SCORE = newScore;
+    document.querySelector(".score h1").innerText = newScore;
 }
